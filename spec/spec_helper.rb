f4d3fcb53bf8349e6dd44c178019647dc3ea5773
Antonio Chavez <cavjzz@gmail.com>
2012-10-12 19:30:25 -0500

    Initialized rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,46 @@
+require 'rubygems'
+require 'spork'
+
+Spork.prefork do
+
+  ENV["RAILS_ENV"] ||= 'test'
+  require File.expand_path("../../config/environment", __FILE__)
+  require 'rspec/rails'
+  require 'rspec/autorun'
+  require 'capybara/rspec'
+  require 'devise'
+  require 'simplecov'
+  SimpleCov.start
+
+  Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+  RSpec.configure do |config|
+    config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+    config.use_transactional_fixtures = true
+
+    config.infer_base_class_for_anonymous_controllers = false
+
+    config.order = "random"
+
+    config.before(:suite) do
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.clean_with(:truncation)
+    end
+
+    config.before(:each) do
+      DatabaseCleaner.start
+    end
+
+    config.after(:each) do
+      DatabaseCleaner.clean
+    end
+
+  end
+
+end
+
+Spork.each_run do
+  # This code will be run each time you run your specs.
+
+end
