d7111c20607d53c68226e0c82ece2c4ecaea180d
Antonio Chavez <cavjzz@gmail.com>
2012-10-13 00:35:43 -0500

    Added acts as api template in the model

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,13 @@
 class Song < ActiveRecord::Base
+
+  acts_as_api
+
   mount_uploader :file, FileUploader
   has_many :playlists, through: :playlist_songs
   has_many :playlist_songs
+
+  api_accessible :music do |t|
+    t.add :name
+    t.add :file
+  end
 end
--- spec/models/song_spec.rb
+++ spec/models/song_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Song do
-  pending 'Add some specs here'
+  before(:each) do
+    @song = FactoryGirl.create(:song)
+    @songs = Song.all
+  end
+  context 'JSON' do
+
+    it 'should return collection of songs as JSON' do
+      (render_for_api :music, json: @songs).first.should be_kind_of ActiveSupport::JSON
+    end
+
+  end
+
 end
