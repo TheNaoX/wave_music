5d517e6b18c68a52fba2bef22225553e36c8ec4f
Antonio Chavez <cavjzz@gmail.com>
2012-10-12 22:18:44 -0500

    Added file uploader

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,5 @@
 class Song < ActiveRecord::Base
+  mount_uploader :file, FileUploader
   has_many :playlists, through: :playlist_songs
   has_many :playlist_songs
 end
---
+++ spec/uploaders/file_uploader_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe FileUploader do
+end
