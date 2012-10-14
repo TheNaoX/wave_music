fde1b1eac886341a1fdb2bac4b52154c17a8f3bd
Antonio Chavez <cavjzz@gmail.com>
2012-10-14 01:36:25 -0500

    Added test and route

--- config/routes.rb
+++ config/routes.rb
@@ -6,20 +6,32 @@ R12Team455::Application.routes.draw do
   root :to => 'sessions#index'
 
   namespace :api do
+
     resources :songs, only: [:index, :show] do
+
+      collection do
+        get :search
+      end
+
       member do
         get :stream
       end
     end
+
     resources :playlists, only: [:index, :show, :create] do
+
       collection do
         get :search
       end
+
       member do
         post :add_songs
       end
+
     end
+
     resources :uploads, only: [:create]
+
   end
 
 end
