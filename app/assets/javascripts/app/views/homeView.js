eede6de1008d345f5d15d5d5c869c0800e5c43f3
Antonio Chavez <cavjzz@gmail.com>
2012-10-14 17:33:41 -0500

    Divide an conquer

--- app/assets/javascripts/app/views/homeView.js
+++ app/assets/javascripts/app/views/homeView.js
@@ -2,8 +2,6 @@ RB.HomeView = Backbone.View.extend({
   el: ".theApp",
 
   events: {
-    "click #fn-upload-send"          : "uploadAudio",
-    "click #fn-create-playlist-send" : "createPlaylist",
     "dblclick .fn-song"            : "addToPlaylist",
     "dblclick .pl-song"              : "stream",
     "keyup #fn-search"               : "search",
@@ -17,10 +15,6 @@ RB.HomeView = Backbone.View.extend({
     this.playlist = new Array();
     this.songs = new RB.SongsCollection();
     this.songs.bind("reset", this.render, this);
-
-    this.upload = new RB.UploadController({
-      collection: this.songs
-    });
 
     this.songs.fetch();
   },
@@ -60,42 +54,6 @@ RB.HomeView = Backbone.View.extend({
     var text = $target.val();
 
     this.songs.search(text);
-  },
-
-  uploadAudio: function(event){
-    event.preventDefault();
-
-    var data = {
-      file: this.$("#fn-upload-file")[0].files.length > 0 ? $('#fn-upload-file')[0].files[0] : null,
-      artist: this.$("#fn-upload-artist").val() !== "" ? this.$("#fn-upload-artist").val() : null,
-      name: this.$("#fn-upload-name").val() !== "" ? this.$("#fn-upload-name").val() : null
-    };
-
-    if(data.file && data.name){
-      this.upload.startUpload(data);
-    }
-  },
-
-  createPlaylist: function(event){
-    event.preventDefault();
-    var name = this.$("#fn-create-playlist-name").val();
-
-    if(name){
-      var newPlaylist = new RB.PlaylistModel({
-        name: name
-      });
-
-      var request = newPlaylist.save();
-
-      request.done(function(data){
-        $("#fn-create-playlist-msg").text(data.message + " id: " + data.playlist);
-      });
-
-      request.fail(function(error, type){
-        $("#fn-create-playlist-msg").text(type);
-      });
-
-    }
   },
 
   stream: function(event){
--- app/assets/javascripts/router.js
+++ app/assets/javascripts/router.js
@@ -1,9 +1,12 @@
 RB.Router = Backbone.Router.extend({
   routes: {
     "": "Home",
-    '/sessions/index': "Upload"
+    "sessions/index": "Upload"
   },
 
+  Upload: function(){
+    this.uploadView = new RB.UploadView();
+  },
   Home: function(){
     this.homeView = new RB.HomeView();
   }
