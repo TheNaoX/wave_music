1bc1a5267448229d59e588f6efa57ef9509064b8
Tonatiuh Nunez <tonatiuhn@hotmail.es>
2012-10-13 23:48:37 -0500

    modified: changed css default format from scss to sass

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ end
 
 module R12Team455
   class Application < Rails::Application
+    config.generators.stylesheet_engine = :sass
+    config.sass.preferred_syntax = :sass
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
