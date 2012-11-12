2cd7fca6b9e72935ad527813aeb7c587497bf18d
Antonio Chavez <cavjzz@gmail.com>
2012-11-12 12:28:52 -0600

    Added authentication token for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ActiveRecord::Base
   validates_presence_of   :username, :email
   validates_uniqueness_of :username, :email
+  before_save :ensure_authentication_token
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :token_authenticatable
 
   attr_accessible :username, :email, :password, :password_confirmation, :remember_me
   has_many :api_accesses
