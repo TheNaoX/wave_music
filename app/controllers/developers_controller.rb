c19506d77cb93807d2f7789cd1dd8cab3b823382
Antonio Chavez <cavjzz@gmail.com>
2012-11-13 12:51:22 -0600

    Finished to add application tokens

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -10,7 +10,7 @@ class DevelopersController < ApplicationController
 
   def create
     @api_access = ApiAccess.new(params[:api_access])
-    if @api_access.save
+    if current_user.api_accesses << @api_access
       redirect_to developers_path
     else
       flash[:alert]='Could not register your application, verify that the name field'
